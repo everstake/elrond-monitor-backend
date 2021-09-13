aa60c7c3cbb1738cccfff27123daf45b963df6cc
Vadym Mozharivsky <vdm@everstake.one>
2021-09-13 14:26:59 +0300

    update ranking

--- services/ranking.go
+++ services/ranking.go
@@ -6,7 +6,6 @@ import (
 	"github.com/everstake/elrond-monitor-backend/log"
 	"github.com/everstake/elrond-monitor-backend/smodels"
 	"github.com/shopspring/decimal"
-	"sort"
 )
 
 func (s *ServiceFacade) GetRanking() (items []smodels.Ranking, err error) {
@@ -23,75 +22,69 @@ func (s *ServiceFacade) MakeRanking() {
 		log.Error("makeRanking: %s", err.Error())
 	}
 }
-
-type delegator struct {
-	address string
-	amount  decimal.Decimal
-}
-
 func (s *ServiceFacade) makeRanking() error {
 	delegations, err := s.dao.GetDelegationState()
 	if err != nil {
 		return fmt.Errorf("dao.GetDelegationState: %s", err.Error())
 	}
-	delegatorsMap := make(map[string]decimal.Decimal)
 	delegationsMap := make(map[string]map[string]decimal.Decimal) // [provider][delegator]amount
 	for _, delegation := range delegations {
-		delegatorsMap[delegation.Delegator] = delegatorsMap[delegation.Delegator].Add(delegation.Amount)
+		if delegation.Amount.IsZero() {
+			continue
+		}
 		if _, ok := delegationsMap[delegation.Validator]; !ok {
 			delegationsMap[delegation.Validator] = make(map[string]decimal.Decimal)
 		}
 		delegationsMap[delegation.Validator][delegation.Delegator] = delegation.Amount
 	}
-	delegators := make([]delegator, 0, len(delegatorsMap))
-	for address, amount := range delegatorsMap {
-		delegators = append(delegators, delegator{
-			address: address,
-			amount:  amount,
-		})
-	}
-	sort.Slice(delegators, func(i, j int) bool {
-		return delegators[i].amount.GreaterThan(delegations[j].Amount)
-	})
-	if len(delegators) > 200 {
-		delegators = delegators[:200]
-	}
-	addressesMap := make(map[string]bool)
-	for _, d := range delegators {
-		addressesMap[d.address] = true
-	}
+
 	var providers []smodels.StakingProvider
 	err = s.getCache(dmodels.StakingProvidersStorageKey, &providers)
 	if err != nil {
 		return fmt.Errorf("getCache: %s", err.Error())
 	}
+
 	if len(providers) > 100 {
 		providers = providers[:100]
 	}
+
 	var ranking []smodels.Ranking
-	for _, provider := range providers {
-		name := provider.Name
-		if name == "" {
-			name = provider.Identity
-		}
-		if name == "" {
-			name = provider.Provider
-		}
-		filteredDelegationsMap := make(map[string]decimal.Decimal)
-		for d, amount := range delegationsMap[provider.Provider] {
-			if addressesMap[d] {
-				filteredDelegationsMap[d] = amount
+	for _, p := range providers {
+		rank := smodels.Ranking{Name: p.Name, Address: p.Provider}
+		total := decimal.Zero
+		for _, amount := range delegationsMap[p.Provider] {
+			switch true {
+			case amount.GreaterThanOrEqual(decimal.Zero) && amount.LessThan(intToDec(100)):
+				rank.T100.Count++
+			case amount.GreaterThanOrEqual(intToDec(100)) && amount.LessThan(intToDec(1000)):
+				rank.F100T1k.Amount = rank.F100T1k.Amount.Add(amount)
+				rank.F100T1k.Count++
+			case amount.GreaterThanOrEqual(intToDec(1000)) && amount.LessThan(intToDec(10000)):
+				rank.F1kT10k.Amount = rank.F1kT10k.Amount.Add(amount)
+				rank.F1kT10k.Count++
+			case amount.GreaterThanOrEqual(intToDec(10000)) && amount.LessThan(intToDec(100000)):
+				rank.F10kT100k.Amount = rank.F10kT100k.Amount.Add(amount)
+				rank.F10kT100k.Count++
+			case amount.GreaterThanOrEqual(intToDec(100000)):
+				rank.F100k.Amount = rank.F100k.Amount.Add(amount)
+				rank.F100k.Count++
 			}
+			total = total.Add(amount)
 		}
-		ranking = append(ranking, smodels.Ranking{
-			Provider:   name,
-			Amount:     provider.Locked,
-			Delegators: filteredDelegationsMap,
-		})
+		if total.GreaterThan(p.Locked) {
+			log.Warn("makeRanking: provider[%s, %s]: total > locked", p.Name, p.Provider)
+			continue
+		}
+		rank.T100.Amount = p.Locked.Sub(total)
+		ranking = append(ranking, rank)
 	}
 	err = s.setCache(dmodels.RankingStorageKey, ranking)
 	if err != nil {
 		return fmt.Errorf("setCache: %s", err.Error())
 	}
 	return nil
+}
+
+func intToDec(v int64) decimal.Decimal {
+	return decimal.New(v, 0)
 }
--- smodels/ranking.go
+++ smodels/ranking.go
@@ -2,8 +2,17 @@ package smodels
 
 import "github.com/shopspring/decimal"
 
+type RankingRange struct {
+	Amount decimal.Decimal `json:"amount"`
+	Count  uint64          `json:"count"`
+}
+
 type Ranking struct {
-	Provider   string                     `json:"provider"`
-	Amount     decimal.Decimal            `json:"amount"`
-	Delegators map[string]decimal.Decimal `json:"delegators"`
+	Name      string       `json:"name"`
+	Address   string       `json:"address"`
+	T100      RankingRange `json:"t_100"`
+	F100T1k   RankingRange `json:"f_100_t_1k"`
+	F1kT10k   RankingRange `json:"f_1k_t_10k"`
+	F10kT100k RankingRange `json:"f_10k_t_100k"`
+	F100k     RankingRange `json:"f_100k"`
 }
